02a5ab4f9ba0c65598aaaa9b1ac768db81540276
Mukesh Soni <mukeshsoni@gmail.com>
2019-12-10 19:56:14 +0530

    roll config

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+const config = {
+  input: 'parser.js',
+  output: {
+    file: 'dist/sketch-systems-parser.umd.js',
+    format: 'umd',
+    name: 'SketchSystemsParser',
+  },
+};
+
+export default config;
