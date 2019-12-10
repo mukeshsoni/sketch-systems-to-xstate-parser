0c5b53aefc8488b0df5ae53bb3a37ce5409dd4b4
Mukesh Soni <mukeshsoni@gmail.com>
2019-12-10 19:56:35 +0530

    let babel convert ES6 to commonjs module for jest tests

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-// tokenizer for indent based language
-// name = n:[a-zA-Z0-9_]+ { return n.join("") }
-// state_name = name
-// transition_name = name
-// newlines = [\n]+
-// Whitespace = [" "\t]*
-// arrow = Whitespace"->"Whitespace
-// parallel_state = "&" 
+const { parse } = require("./parser");
 
-const tokenize = require('./tokenizer');
-const { parse } = require('./parser');
+module.exports = parse;
 
-const inputStr = `abc
-  def -> lmn
-  pasta -> noodles
-  ast&
-    opq -> rst
-    uvw -> xyz
-  try -> this`;
-
-console.log(inputStr);
-
-const tokens = tokenize(inputStr);
-
-// console.log(tokens.filter(token => token.type !== 'WS'))
-
-const ast = parse(tokens);
-
-console.log('ast', JSON.stringify(ast, null, 2));
--- parser.test.js
+++ parser.test.js
@@ -1,5 +1,5 @@
-const { tokenize } = require("./tokenizer");
-const { parse } = require("./parser");
+import { tokenize } from './tokenizer';
+import { parse } from './parser';
 
 const inputStr = `abc
   def -> lmn
@@ -12,25 +12,25 @@ const inputStr = `abc
 
 const expectedXstateJSON = {
   abc: {
-    type: "sequential",
+    type: 'sequential',
     on: {
-      def: "lmn",
-      pasta: "noodles",
-      tried: "that"
+      def: 'lmn',
+      pasta: 'noodles',
+      tried: 'that',
     },
     states: {
       ast: {
-        type: "parallel",
+        type: 'parallel',
         on: {
-          opq: "rst",
-          uvw: "xyz"
-        }
+          opq: 'rst',
+          uvw: 'xyz',
+        },
       },
       lastState: {
-        type: "sequential"
-      }
-    }
-  }
+        type: 'sequential',
+      },
+    },
+  },
 };
 
 const invalidInputStr = `abc
@@ -38,58 +38,55 @@ const invalidInputStr = `abc
       pqr
     stm`;
 
-describe("tokenizer", () => {
-  it("should give the correct number of tokens", () => {
+describe('tokenizer', () => {
+  it('should give the correct number of tokens', () => {
     const tokens = tokenize(inputStr);
 
     expect(tokens).toHaveLength(30);
   });
 
-  it("gives correct indent and dedent tokens", () => {
+  it('gives correct indent and dedent tokens', () => {
     const tokens = tokenize(inputStr);
 
-    expect(tokens[2].type).toEqual("INDENT");
-    expect(tokens[14].type).toEqual("INDENT");
-    expect(tokens[23].type).toEqual("DEDENT");
-    expect(tokens[28].type).toEqual("DEDENT");
+    expect(tokens[2].type).toEqual('INDENT');
+    expect(tokens[14].type).toEqual('INDENT');
+    expect(tokens[23].type).toEqual('DEDENT');
+    expect(tokens[28].type).toEqual('DEDENT');
   });
 
-  it("catches incorrect indentation errors", () => {
-    expect(() => tokenize(invalidInputStr)).toThrowError("Invalid indentation");
+  it('catches incorrect indentation errors', () => {
+    expect(() => tokenize(invalidInputStr)).toThrowError('Invalid indentation');
   });
 
-  it("should have line and column number for tokens", () => {
+  it('should have line and column number for tokens', () => {
     const tokens = tokenize(inputStr);
 
     const secondIdentifier = tokens[3];
 
-    expect(secondIdentifier.type).toEqual("IDENTIFIER");
+    expect(secondIdentifier.type).toEqual('IDENTIFIER');
     expect(secondIdentifier.line).toEqual(2);
     expect(secondIdentifier.col).toEqual(3);
 
     const lastToken = tokens[29];
 
-    expect(lastToken.type).toEqual("IDENTIFIER");
+    expect(lastToken.type).toEqual('IDENTIFIER');
     expect(lastToken.line).toEqual(8);
     expect(lastToken.col).toEqual(1);
 
-    const uvwToken = tokens.find(token => token.text === "uvw");
+    const uvwToken = tokens.find(token => token.text === 'uvw');
 
-    expect(uvwToken.type).toEqual("IDENTIFIER");
+    expect(uvwToken.type).toEqual('IDENTIFIER');
     expect(uvwToken.line).toEqual(6);
     expect(uvwToken.col).toEqual(5);
   });
 });
 
-describe.only("parser", () => {
-  const tokens = tokenize(inputStr);
-
-  // console.log(tokens.filter(token => token.type !== 'WS'))
-
-  const ast = parse(tokens);
+describe.only('parser', () => {
+  const ast = parse(inputStr);
 
   console.log(JSON.stringify(ast, null, 2));
   expect(ast).toEqual(expectedXstateJSON);
+
   // console.log("AST:\n", JSON.stringify(ast, null, 2));
   expect(true).toBe(true);
 });
